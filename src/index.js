adc4484349187117d6b86c1e71695d512617d556
Kalita Mikhail <mkalita1991@gmail.com>
2018-10-06 00:49:17 +0300

    more readable

--- src/index.js
+++ src/index.js
@@ -1,43 +1,32 @@
 module.exports = function longestConsecutiveLength(array) {
 	
-if (array.length == 0) {
-        return 0;
-    }	
-	
-let max = 1;
+  if (array.length == 0) return 0;
+  
+  let max = 1;
+  let LongestConsecutiveSequence = {};
 
-LongestConsecutiveSequence = {};
+  for (let i=0; i < array.length; i++)
+    LongestConsecutiveSequence[array[i]] = true;
 
-for ( let i=0; i < array.length; i++ )
-        LongestConsecutiveSequence[array[i]] = true;
-
-for ( let i=0; i < array.length; i++ )
-
-{ 
-   if (LongestConsecutiveSequence[array[i]]) 
-    {  
-         let left = array[i] - 1;
-         let right = array[i] + 1;
-         let count = 1;
+  for (let i=0; i < array.length; i++){ 
+    if (LongestConsecutiveSequence[array[i]]){  
+      let left = array[i] - 1;
+      let right = array[i] + 1;
+      let count = 1;
  
-         while (LongestConsecutiveSequence[left]) {
+      while (LongestConsecutiveSequence[left]){
             count++;
             LongestConsecutiveSequence[left] = false;
             left--;
-          }
+      }
  
-        while (LongestConsecutiveSequence[right]) {
+      while (LongestConsecutiveSequence[right]){
             count++;
             LongestConsecutiveSequence[right] = false;
             right++;
-        }
-    
-        max = Math.max(count, max);
-	}
-
-	
-}
-	
-return max;
-	
+      }
+      max = Math.max(count, max);
+    }
+  }
+  return max;
 }
